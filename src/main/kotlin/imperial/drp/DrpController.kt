741498f4c30fec422a3fdad7e20beaef31f78c17
Haowei Hu <hh2119@imperial.ac.uk>
2021-06-07 00:12:16 +0800

    Finish login page

--- src/main/kotlin/imperial/drp/DrpController.kt
+++ src/main/kotlin/imperial/drp/DrpController.kt
@@ -4,17 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.CookieValue
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletResponse
 
 @Controller
 class DrpController {
     @Autowired
     private val homeworkRepository: HomeworkRepository? = null
+
     @RequestMapping("/app")
-    fun app(@CookieValue(value = "user", required = false) user: String, model: Model): String {
-        model.addAttribute("user", user)
-        val homeworks = homeworkRepository!!.findByTutee(user)
-        model.addAttribute("homeworks", homeworks)
+    fun app(@CookieValue(value = "username", required = false) username: String?, model: Model): String {
+        model.addAttribute("username", username)
+        if (username != null) {
+            val homeworks = homeworkRepository!!.findByTutee(username)
+            model.addAttribute("homeworks", homeworks)
+        }
         return "app"
+    }
+
+    @PostMapping("/login")
+    fun login(@RequestParam(value = "username") username: String, response: HttpServletResponse, model: Model): String {
+        val cookie = Cookie("username", username)
+        response.addCookie(cookie)
+        return "login"
     }
 }
