4c6628b4b131a497fd1595dcaf06932c28091c2b
Haowei Hu <hh2119@imperial.ac.uk>
2021-06-06 17:42:16 +0800

    Test failing the CI/CD

--- src/test/kotlin/imperial/drp/DrpApplicationTests.kt
+++ src/test/kotlin/imperial/drp/DrpApplicationTests.kt
@@ -1,13 +1,15 @@
 package imperial.drp
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class DrpApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+        fail { "fail" }
+    }
 
 }
