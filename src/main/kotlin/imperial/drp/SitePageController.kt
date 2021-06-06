21c14fb83f2991927d6315e70f4795e5d86168a3
Krishan Sritharar <ks1619@ic.ac.uk>
2021-06-06 11:55:59 +0100

    Served homepage and 3 sub-pages with styling

---
+++ src/main/kotlin/imperial/drp/SitePageController.kt
@@ -0,0 +1,31 @@
+package imperial.drp
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+class SitePageController {
+
+    @RequestMapping("/")
+    fun homePage() : String {
+        return "index"
+    }
+
+    @RequestMapping("/videoCall")
+    fun videoCallPage() : String {
+        return "videoCall"
+    }
+
+    @RequestMapping("/voiceCall")
+    fun voiceCallPage() : String {
+        return "voiceCall"
+    }
+    @RequestMapping("/textChat")
+    fun textChatPage() : String {
+        return "textChat"
+    }
+
+
+
+
+}
